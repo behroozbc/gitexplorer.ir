a3fb8d4616bc40f1d32153d575d738594ab1350e
behrooz bozorg chami <behroozbozorgchami@hotmail.com>
2021-07-23 18:37:21 +0430

    remove all

--- src/data/index.js
+++
@@ -1,3 +0,0 @@
-export { primaryOptions as optionsFirst } from './primary-options';
-export { secondaryOptions_fa as optionsSecond } from './secondary-options';
-export { tertiaryOptions as optionsThird } from './tertiary-options';
